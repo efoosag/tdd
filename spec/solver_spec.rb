116f15c775916b55742387bb1e2db73b86cbfc54
efoosag <96612103+efoosag@users.noreply.github.com>
2022-09-08 09:33:33 +0100

    add tdd test cases

---
+++ spec/solver_spec.rb
@@ -0,0 +1,40 @@
+require './solver'
+
+describe 'Solver' do
+  context 'Test Solver Class' do
+    it 'factorial 0 to be 1' do
+      solve = Solver.new
+      expect(solve.factorial(0)).to be 1
+    end
+
+    it 'factorial 4 to be 24' do
+      solve = Solver.new
+      expect(solve.factorial(24)).to be 24
+    end
+
+    it 'factorial -1 to be false' do
+      solve = Solver.new
+      expect(solve.factorial(-1)).to be false
+    end
+
+    it 'reverse boy to be yob' do
+      solve = Solver.new
+      expect(solve.reverse('boy')).to eq 'yob'
+    end
+
+    it 'fizzbuzz 3 to be fizz' do
+      solve = Solver.new
+      expect(solve.fizzbuzz(3)).to eq 'fizz'
+    end
+
+    it 'fizzbuzz 5 to be buzz' do
+      solve = Solver.new
+      expect(solve.fizzbuzz(5)).to eq 'buzz'
+    end
+
+    it 'fizzbuzz 3 and 5 to be buzz' do
+      solve = Solver.new
+      expect(solve.fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+  end
+end
