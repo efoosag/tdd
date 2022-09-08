4bd6c135c4cedd1e027392027cb14d96358ed50d
udeaghad <dozie.udeagha@gmail.com>
2022-09-08 13:02:16 +0100

    Write method for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -10,6 +10,15 @@ class Solver
     str.reverse()
   end
 
-  def fizzbuzz(n)
+  def fizzbuzz(n)    
+    if n%3 == 0 && n%5 == 0
+      "fizzbuzz"
+    elsif n%3 == 0
+      "fizz"
+    elsif n%5 == 0
+      "buzz"
+    else
+      n
   end
+end
 end
